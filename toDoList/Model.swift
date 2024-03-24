e58f7800fe80d043b451c92e8556278aaf077f8e
valeriia <novikovavaleria994@gmail.com>
2024-03-24 19:11:43 +0100

    add app icon & badge

--- toDoList/Model.swift
+++ toDoList/Model.swift
@@ -6,11 +6,14 @@
 //
 
 import Foundation
+import UserNotifications
+import UIKit
 
 var toDoItems: [[String : Any]] {
     set {
         UserDefaults.standard.set(newValue, forKey: "toDoDataKey")
         UserDefaults.standard.synchronize()
+        setBadge()
     }
     get {
         if let array = UserDefaults.standard.array(forKey: "toDoDataKey") as? [[String : Any]] {
@@ -38,4 +41,23 @@ func moveItem(from: Int, to: Int) {
     let moveItem = toDoItems[from]
     toDoItems.remove(at: from)
     toDoItems.insert(moveItem, at: to)
+}
+
+func requestForNotificatios() {
+    UNUserNotificationCenter.current().requestAuthorization(options: [.badge]) { isEnabled, error in
+        if isEnabled {
+            print("Согласие получено")
+        } else {
+            print("Согласие не получено")
+        }
+    }
+}
+
+func setBadge() {
+    var badgeCount = 0
+    for item in toDoItems {
+        if item["isCompleted"] as! Bool == false { badgeCount += 1 }
+    }
+    
+    UNUserNotificationCenter.current().setBadgeCount(badgeCount)
 }
